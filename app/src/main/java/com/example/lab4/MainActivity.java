249c63a0e7c37b158b013f74f8c48c16380d6ff0
lakashmaharjan <lakashmaharjan@gmail.com>
2021-03-17 20:50:50 +0545

    data send form one activity to another activity

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.lab4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    //private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    EditText mMessageEditText;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // hooks
+        mMessageEditText = findViewById(R.id.editText_main);
+        btn = findViewById(R.id.button_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String message = mMessageEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this,activity_second.class);
+                intent.putExtra("message",message);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void launchSecondLayout(View view) {
-        Log.d(LOG_TAG,"Button Clicked!!");
-        Intent intent =new Intent(this, activity_second.class);
-        startActivity(intent);
-    }
+
+
 }
--- app/src/main/java/com/example/lab4/activity_second.java
+++ app/src/main/java/com/example/lab4/activity_second.java
@@ -2,13 +2,21 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_second extends AppCompatActivity {
 
+    TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        message = findViewById(R.id.getMessage);
+        Intent intent = getIntent();
+        String messages = intent.getStringExtra("message");
+        message.setText(messages);
+
     }
 }
